fbe8ab286c5416e7597f0873f732c7d47314ef2d
Patrick <patrick.czaczka98@gmail.com>
2022-07-29 10:35:18 +1000

    merge 3

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 console.log("Hello world");
 
 function adding(x,y) {
@@ -6,7 +5,7 @@ function adding(x,y) {
 }
 
 adding(5,10);
-=======
+
 console.log("I caused a merge conflict");
+
 console.log("for fun");
->>>>>>> b5371376bcbe5e1375363192b813b9e7f679b62c
